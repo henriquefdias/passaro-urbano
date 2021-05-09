9a0edabd7fdd64adacab7049189803260db692d1
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-05-09 16:46:35 -0300

    Continuação de estudos de Observables

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
-import { interval } from 'rxjs';
+import { interval, Observable, Observer } from 'rxjs';
 import { OfertasService } from "../ofertas.service";
 import { Oferta } from '../shared/oferta.model';
 
@@ -30,12 +30,23 @@ export class OfertaComponent implements OnInit {
       () => console.log("Processamento classificado como concluído.")
     )
     */
-
+    /*
     let tempo = interval(2000)
 
     tempo.subscribe((intervalo: number) => {
       console.log(intervalo)
     })
+    */
+
+    // observable (observavel)
+    let meuObservableTeste = new Observable((observer: Observer<string>) => {
+      observer.next('Primeiro evento da stream')
+    })
+
+    // observable (observador)
+    meuObservableTeste.subscribe(
+      (resultado: any) => console.log(resultado)
+    )
     
   }
 
