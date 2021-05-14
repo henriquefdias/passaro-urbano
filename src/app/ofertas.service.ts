ed0af37208db946844926de379000d6cf1da61c1
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-05-14 09:48:46 -0300

    Início da implementação da pesquisa de ofertas

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Oferta } from './shared/oferta.model';
 import { URL_API } from "./app.api";
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
 //import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -43,6 +45,11 @@ export class OfertasService {
 			.then((resposta: any) => {
 				return resposta[0].descricao
 			})
+	}
+
+	public pesquisaOfertas(termo: string): Observable<Oferta[]> {
+		return this.http.get(`${URL_API}/ofertas?descricao_oferta=${termo}`)
+			.pipe(map((resposta: any) => resposta.json()))
 	}
 
 	/*
