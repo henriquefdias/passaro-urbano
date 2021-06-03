0222301f1dbf2c3ddbf4636393b6ccbf92861dd1
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-06-03 09:20:26 -0300

    Configuração e aplicação do pipe currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { RouterModule } from "@angular/router";
@@ -15,7 +15,10 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { registerLocaleData } from "@angular/common";
+import localePt from "@angular/common/locales/pt";
 
+registerLocaleData(localePt);
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,7 +37,7 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [ {provide: LOCALE_ID, useValue: 'pt'} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
