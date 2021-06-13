b786f7cd23cd1c779d73aa919daab558782a90d9
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-06-12 22:48:09 -0300

    Adição do componente Ordem Compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import localePt from "@angular/common/locales/pt";
 
 // pipe
 import { DescricaoReduzida } from "./util/descricao-reduzida.pipe";
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -33,7 +34,8 @@ registerLocaleData(localePt);
     OfertaComponent,
     ComoUsarComponent,
     OndeFicaComponent,
-    DescricaoReduzida
+    DescricaoReduzida,
+    OrdemCompraComponent
   ],
   imports: [
     BrowserModule,
