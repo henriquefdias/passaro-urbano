3937cc7fdb613a882fb220170b0105fbd634a057
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-06-25 05:47:34 -0300

    Injetando http no service ordem-compra

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,6 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
 import { Pedido } from "./shared/pedido.model";
 
+@Injectable()
 export class OrdemCompraService {
+
+    constructor(private httpClient: HttpClient) {}
+
     public efetivarCompra(pedido: Pedido): void {
         console.log(pedido)
     }
