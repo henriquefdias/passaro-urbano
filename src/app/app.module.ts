014e12d2569471511d4c965fc60fb851ffe8dade
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-06-29 07:28:10 -0300

    Preparo do projeto para uso de Template Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import localePt from "@angular/common/locales/pt";
 import { DescricaoReduzida } from "./util/descricao-reduzida.pipe";
 import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
 import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra-sucesso.component';
+import { FormsModule } from '@angular/forms';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -43,6 +44,7 @@ registerLocaleData(localePt);
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [ {provide: LOCALE_ID, useValue: 'pt'} ],
