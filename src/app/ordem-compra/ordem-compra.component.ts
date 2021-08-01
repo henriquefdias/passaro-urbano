de48cdfb20fcab2d9bbf9c21ca90bca4ca9d12b8
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-07-31 22:46:51 -0300

    Validação em ordem compra
    Validação em ordem compra que impede confirmação do pedido caso carrinho esteja vazio

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -34,24 +34,26 @@ export class OrdemCompraComponent implements OnInit {
 
   public confirmarCompra(): void {
     if(this.formulario.status === 'INVALID') {
-      console.log("Formulário Inválido")
       this.formulario.get('endereco').markAsTouched()
       this.formulario.get('numero').markAsTouched()
       this.formulario.get('complemento').markAsTouched()
       this.formulario.get('formaPagamento').markAsTouched()
     } else {
+      if(this.carrinhoService.exibirItens().length === 0){
+        alert("Você não possui nenhum item no carrinho!");
+      } else {
       let pedido: Pedido = new Pedido(
         this.formulario.value.endereco,
         this.formulario.value.numero,
         this.formulario.value.complemento,
         this.formulario.value.formaPagamento
       )
-
       this.ordemCompraService.efetivarCompra(pedido)
         .subscribe((idPedido: number) => {
           this.idPedidoCompra = idPedido
           console.log(this.idPedidoCompra)
         })
+      }
     }
   }
 
